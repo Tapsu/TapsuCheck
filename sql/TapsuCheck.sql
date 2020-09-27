db3893925526840a46f7db53c7de54ae17a603dc
Tapsu <44003337+Tapsu@users.noreply.github.com>
2020-09-27 12:17:23 +0300

    Update TapsuCheck.sql

--- sql/TapsuCheck.sql
+++ sql/TapsuCheck.sql
@@ -6,7 +6,3 @@ CREATE TABLE `whitelist` (
 CREATE TABLE `poikkeusluvat` (
   `identifier` varchar(60) COLLATE utf8mb4_bin NOT NULL, PRIMARY KEY (`identifier`)
 );
-
-INSERT INTO `poikkeusluvat` (`identifier`) VALUES
-	('steam:1100001155db950'),
-	('steam:11000013de6363f');
